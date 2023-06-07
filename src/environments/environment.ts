5040eb22f38157a49277ea7978fa9af18cb10a4a
Punit <punitjnva123@gmail.com>
2023-06-08 02:28:13 +0530

    WeatherApp moving to git repo

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'cbf48e6cf2msh996d5e7f00dab65p1943ebjsnea15acf1e74c',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+}
